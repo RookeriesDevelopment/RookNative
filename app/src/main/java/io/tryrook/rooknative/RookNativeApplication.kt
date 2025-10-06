5e2036a2281834e8de69c1eb4f16ba8b832dbe2d
dnieln7 <dnieln7@gmail.com>
2025-10-06 16:19:41 -0600

    tutorial 3.5: connections - onCreate

--- app/src/main/java/io/tryrook/rooknative/RookNativeApplication.kt
+++ app/src/main/java/io/tryrook/rooknative/RookNativeApplication.kt
@@ -2,15 +2,37 @@ package io.tryrook.rooknative
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.tryrook.rooknative.core.domain.preferences.AppPreferences
+import io.tryrook.rooknative.core.framework.health.RookHealthConnectRepository
+import io.tryrook.rooknative.core.framework.health.RookSamsungHealthRepository
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class RookNativeApplication : Application() {
+
+    @Inject
+    lateinit var appPreferences: AppPreferences
+
+    @Inject
+    lateinit var healthConnectRepository: RookHealthConnectRepository
+
+    @Inject
+    lateinit var samsungHealthRepository: RookSamsungHealthRepository
+
     override fun onCreate() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+
+        if (appPreferences.isHealthConnectEnabled()) {
+            healthConnectRepository.schedule(BuildConfig.DEBUG)
+        }
+
+        if (appPreferences.isSamsungHealthEnabled()) {
+            samsungHealthRepository.schedule(BuildConfig.DEBUG)
         }
     }
 }
